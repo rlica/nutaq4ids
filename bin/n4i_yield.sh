e35096b09ab4afa3a5f7df106b6e2ff29f73271d
rlica <razvan.lica@cern.ch>
2017-05-28 20:13:26 +0200

    Yield

--- bin/n4i_yield.sh
+++ bin/n4i_yield.sh
@@ -5,7 +5,7 @@
 ##############################
 
 
-NUMBER_OF_BLOCKS=50           
+NUMBER_OF_BLOCKS=100           
 WRITE_ROOT='y'
 
 RAW_PATH='/home/TapeData/IS633/'
@@ -14,7 +14,7 @@ CONFIG_FILE='/home/idsuser/Experiments/2017/IS633/root/conf/config_rate'
 CAL_FILE='/home/idsuser/Experiments/2017/IS633/root/conf/EClovers_152Eu.cal'   
 ROOT_MACRO='/home/idsuser/Experiments/2017/IS633/root/macros/yield.C'   
 
-MESSAGE='IS633 - Yield of 511 keV (cps) = '
+MESSAGE='Yield of 511 keV (cps) = '
 
 
 ##############################
@@ -41,25 +41,38 @@ $WRITE_ROOT
 echo
 
 
+sleep 1
+
 #RUNNING ROOT MACRO
 root -b -l -q $ROOT_MACRO"(0)"
 
 
 #EXTRACTING the YIELD 
-YIELD=$(cat $EXP_PATH/out0.txt)
+YIELD511=$(awk '{print $1}' "$EXP_PATH/out0.txt")
+YIELD1461=$(awk '{print $2}' "$EXP_PATH/out0.txt")
+
 echo "============================================"
-tput setaf 2; echo " $MESSAGE  $YIELD "
+tput setaf 2; echo " Yield of 511 keV (cps) =   $YIELD511"
+tput setaf 2; echo " Yield of 1461 keV (cps) =   $YIELD1461"
 tput sgr0
 echo "============================================"
 
-#SENDING DATA TO GRAFANA
-curl --user is633:37TzGnFdBgw9 -i -XPOST 'http://influx.kern.phys.au.dk/write?db=is633' --data-binary "nutaq,E=511 count=$YIELD" >> /dev/null 
+
+    #bash handles only integer maths but you can use bc
+
+	if (( $(echo "$YIELD511 > 0" |bc -l)  )); then
+	#SENDING DATA TO GRAFANA
+	curl --user is633:xNnZcCXr3PJE -i -XPOST 'http://influx.kern.phys.au.dk/write?db=is633'	--data-binary "nutaq,E=511 count=$YIELD511" >> /dev/null 
+	curl --user ids:yrYm3HDLCfVN -i -XPOST 'http://influx.kern.phys.au.dk/write?db=ids' --data-binary "nutaq,E=511 count=$YIELD511" >> /dev/null 
+	curl --user ids:yrYm3HDLCfVN -i -XPOST 'http://influx.kern.phys.au.dk/write?db=ids' --data-binary "nutaq,E=1461 count=$YIELD1461" >> /dev/null 
+
+	fi
+	
 
 
 printf "$(date)    $(du -h $INPUT_FILE) \n"
-sleep 3
 OLD_FILE_SIZE=$NEW_FILE_SIZE
-
+sleep 10
 
     	
    else
@@ -68,5 +81,6 @@ printf "$(date)    Waiting for file update ... \r"
 sleep 1
 
  fi
+ 
 
 done
